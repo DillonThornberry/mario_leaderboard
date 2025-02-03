649b6df5979119f14e36235948fec8fa151d98bc
DillonThornberry <54234906+DillonThornberry@users.noreply.github.com>
2025-02-02 18:49:45 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+var timeInSeconds = time => {
+    var timeArray = time.split(":");
+    return parseInt(timeArray[0]) * 60 + parseInt(timeArray[1]);
+}
+
+var players = {
+    sixsix229: {
+        name: "Sixsix229",
+        time: "18:52",
+        url: "https://www.twitch.tv/sixsix229"
+    },
+    nation_live: {
+        name: "nation_live",
+        time: "17:27",
+        url: "https://www.twitch.tv/nation_live"
+    },
+    smithjrblaqualuigi: {
+        name: "SmithJrBlaquaLuigi",
+        time: "16:57",
+        url: "https://www.twitch.tv/smithjrblaqualuigi"
+    },
+}
